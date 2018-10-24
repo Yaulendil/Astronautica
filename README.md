c350e2b47d027eaf641eb8989c820bcdb9136721
Davarice <jsdreim@gmail.com>
2018-10-24 14:26:43 -0400

    Cleaned up math

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ In actuality, it will be turn-based, but with turns at strict intervals in real 
 A Host instance must run separately from the Client instances that connect to the Host. The Host will manage the game world and control turns ticking over, as well as AI crewmembers.
 
 Client instances will connect to the Host of a game session, and give directions to their vessel. The directions will be added to a List, and executed in order at the next game tick.
+
+### Coordinates
+
+From [Wikipedia](https://en.wikipedia.org/wiki/Spherical_coordinate_system#Conventions) (slightly clipped):
+
+coordinates|corresponding local geographical directions (Z, X, Y)|right/left-handed
+---:|:---:|---
+(*r*, *θ* elevation, *φ* azimuth,right)|(U, N, E)|left
+`Note: easting (E), northing (N), upwardness (U). Local azimuth angle would be measured, e.g., counterclockwise from S to E in the case of (U, S, E).`
+
+This pre-existing coordinate system has been chosen because:
+1. An angle where *φ*=0 is straight ahead,
+2. Positive values of *φ* increase clockwise, and
+3. An angle where *θ*=0 is on a flat plane.
--- geometry.py
+++ geometry.py
@@ -2,13 +2,13 @@ from astropy import units
 import numpy
 
 
-def cart_polar(x, y):
+def cart2_polar2(x, y):
     rho = numpy.sqrt(x ** 2 + y ** 2)
     phi = numpy.arctan2(y, x).to(units.degree)
     return rho, phi
 
 
-def polar_cart(rho, phi):
+def polar2_cart2(rho, phi):
     x = rho * numpy.cos(phi)
     y = rho * numpy.sin(phi)
     return x, y
@@ -18,17 +18,28 @@ class Coordinates:
     """Coordinates object:
     Store coordinates as a cartesian tuple and return transformations as requested"""
 
-    def __init__(self, x=0, y=0, z=0):
-        self.cartesian = (x * units.kilometer, y * units.kilometer, z * units.kilometer)
+    def __init__(self, *, car=None, cyl=None, pol=None):
+        if car and len(car) >= 3:  # CARTESIAN: (X, Y, Z)
+            self.cartesian = (
+                car[0] * units.kilometer,
+                car[1] * units.kilometer,
+                car[2] * units.kilometer,
+            )
+        elif cyl and len(cyl) >= 3:  # CYLINDRICAL: (R, φ azimuth, Y)
+            pass
+        elif pol and len(pol) >= 3:  # SPHERICAL: (R, θ elevation, φ azimuth)
+            pass
+        else:
+            raise TypeError("Coordinates object requires initial values")
 
         self.heading = (
             0 * units.degree,
             0 * units.degree,
-        )  # (azimuth, altitude); Direction object is FACING
+        )  # (θ elevation, φ azimuth); Direction object is FACING
         self.course = (
             0 * units.degree,
             0 * units.degree,
-        )  # (azimuth, altitude); Direction object is MOVING
+        )  # (θ elevation, φ azimuth); Direction object is MOVING
         self.velocity = 0 * (units.meter / units.second)
         # Velocity can be considered the rho of the course
 
@@ -41,21 +52,30 @@ class Coordinates:
     def c_cyl(self):
         """Return the CYLINDRICAL coordinates"""
         x, y, z = self.cartesian  # Take the cartesian coordinates
-        rho, phi = cart_polar(x, z)  # Find rho and phi of x and z
+        rho, phi = cart2_polar2(x, z)  # Find rho and phi of x and z
         return rho, phi, y  # Return rho, phi, and height
 
     @property
     def c_pol(self):
         """Return the SPHERICAL coordinates (horizontal)"""
         x, y, z = self.cartesian
-        rho0, phi = cart_polar(x, y)
-        rho, theta = cart_polar(z, rho0)
-        return rho, phi, theta
+        rho0, phi = cart2_polar2(x, y)
+        rho, theta = cart2_polar2(z, rho0)
+        return rho, theta, phi
 
 
 def get_bearing(a, b):
     """Return SPHERICAL coordinates (horizontal) of relative position"""
-    pass
+    ap = a.c_pol  # Polar of A
+    ac = a.c_car  # Cartesian of A
+    bp = b.c_pol  # Polar of B
+    bc = b.c_car  # Cartesian of B
+    ab_r = numpy.sqrt(
+        (ac[0] - bc[0]) ** 2 + (ac[1] - bc[1]) ** 2 + (ac[2] - bc[2]) ** 2
+    )  # Rho of output
+    ab_tp = ap[1] - bp[1], ap[2] - bp[2]  # Theta and Phi of output
+    ab = ab_r, *ab_tp
+    return ab
 
 
 def get_relative(a, b):
