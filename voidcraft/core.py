f2b17b1a0d6db0a8ac4df7180be3ea34164bb267
Davarice <jsdreim@gmail.com>
2018-10-23 20:41:07 -0400

    Basics of space objects

--- voidcraft/core.py
+++ voidcraft/core.py
@@ -1,3 +1,30 @@
+from astropy import units, constants
+import numpy
+
+
+def get_bearing(a, b):
+    pass
+
+
+class ObjectInSpace:
+    def __init__(self, x=0, y=0, z=0):
+        self.coordinates = (
+            x * units.kilometer,
+            y * units.kilometer,
+            z * units.kilometer,
+        )
+        self.velocity = 0 * (units.meter / units.second)
+
+        self.heading = (
+            0 * units.rad,
+            0 * units.rad,
+        )  # (azimuth, altitude); Direction object is FACING
+        self.course = (
+            0 * units.rad,
+            0 * units.rad,
+        )  # (azimuth, altitude); Direction object is MOVING
+
+
 class Department:
     def __init__(self, size, maximum=None):
         if maximum is None:
