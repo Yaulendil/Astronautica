2401ddd4e5163d45ee357d83ec34ecb03be472c0
Davarice <jsdreim@gmail.com>
2018-10-23 23:37:58 -0400

    Math work
    Slight Black compliance change

--- geometry.py
+++ geometry.py
@@ -1,20 +1,51 @@
-from astropy import units, constants
+from astropy import units
 import numpy
 
 
+def cart_polar(x, y):
+    rho = numpy.sqrt(x ** 2 + y ** 2)
+    phi = numpy.arctan2(y, x).to(units.degree)
+    return rho, phi
+
+
+def polar_cart(rho, phi):
+    x = rho * numpy.cos(phi)
+    y = rho * numpy.sin(phi)
+    return x, y
+
+
 class Coordinates:
+    """Coordinates object:
+    Store coordinates as a cartesian tuple and return transformations as requested"""
+
     def __init__(self, x=0, y=0, z=0):
         self.cartesian = (x * units.kilometer, y * units.kilometer, z * units.kilometer)
 
+        self.heading = (
+            0 * units.degree,
+            0 * units.degree,
+        )  # (azimuth, altitude); Direction object is FACING
+        self.course = (
+            0 * units.degree,
+            0 * units.degree,
+        )  # (azimuth, altitude); Direction object is MOVING
+        self.velocity = 0 * (units.meter / units.second)
+        # Velocity can be considered the rho of the course
+
     @property
     def c_pol(self):
-        """Return the SPHERICAL coordinates"""
-        return
+        """Return the SPHERICAL coordinates (horizontal)"""
+        x, y, z = self.cartesian
+        rho0, phi = cart_polar(x, y)
+        rho, theta = cart_polar(z, rho0)
+        return rho, phi, theta
 
     @property
     def c_cyl(self):
         """Return the CYLINDRICAL coordinates"""
-        return
+        x, y, z = self.cartesian  # Take the cartesian coordinates
+        rho, phi = cart_polar(x, z)  # Find rho and phi of x and z
+        return rho, phi, y  # Return rho, phi, and height
 
     @property
     def c_car(self):
@@ -23,7 +54,7 @@ class Coordinates:
 
 
 def get_bearing(a, b):
-    """Return SPHERICAL coordinates of relative position"""
+    """Return SPHERICAL coordinates (horizontal) of relative position"""
     pass
 
 
--- voidcraft/core.py
+++ voidcraft/core.py
@@ -4,21 +4,11 @@ import numpy
 from . import interior
 from .. import geometry
 
+
 class ObjectInSpace:
     def __init__(self, x=0, y=0, z=0, size=100):
         self.radius = size * units.meter  # Assume a spherical cow in a vacuum...
         self.coordinates = geometry.Coordinates(x, y, z)
-
-        self.heading = (
-            0 * units.degree,
-            0 * units.degree,
-        )  # (azimuth, altitude); Direction object is FACING
-        self.course = (
-            0 * units.degree,
-            0 * units.degree,
-        )  # (azimuth, altitude); Direction object is MOVING
-        self.velocity = 0 * (units.meter / units.second)
-        # Velocity can be considered the rho of the course
 
     def tick_movement(self):
         pass
