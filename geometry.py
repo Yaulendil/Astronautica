8db37df40441f20a37bdf47e76c4cd3c49e29d1e
Davarice <jsdreim@gmail.com>
2018-10-24 20:45:36 -0400

    Spherical input support

--- geometry.py
+++ geometry.py
@@ -1,27 +1,42 @@
-from astropy import units
-import numpy
+from astropy import units as u
+import numpy as np
+
+Precision = 5
 
 # CONSTANT DIRECTIONS
 # (θ elevation, φ azimuth)
-NORTH = (0 * units.degree, 0 * units.degree)
-EAST = (0 * units.degree, 90 * units.degree)
-WEST = (0 * units.degree, -90 * units.degree)
-SOUTH = (0 * units.degree, 180 * units.degree)
+NORTH = (0 * u.degree, 0 * u.degree)
+EAST = (0 * u.degree, 90 * u.degree)
+WEST = (0 * u.degree, -90 * u.degree)
+SOUTH = (0 * u.degree, 180 * u.degree)
 
-ZENITH = (90 * units.degree, 0 * units.degree)
-NADIR = (-90 * units.degree, 0 * units.degree)
+ZENITH = (90 * u.degree, 0 * u.degree)
+NADIR = (-90 * u.degree, 0 * u.degree)
 
 
 def cart2_polar2(x, y):
-    rho = numpy.sqrt(x ** 2 + y ** 2)
-    phi = numpy.arctan2(y, x).to(units.degree)
+    rho = np.sqrt(x ** 2 + y ** 2)
+    phi = np.arctan2(y, x)
+    try:
+        phi = phi.to(u.degree)
+    except:
+        pass
     return rho, phi
 
 
 def polar2_cart2(rho, phi):
-    x = rho * numpy.cos(phi)
-    y = rho * numpy.sin(phi)
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
     return x, y
+
+
+def polar3_cart3(rho, theta, phi):
+    theta = np.pi / 2 - theta / 57.2958
+    phi = phi / 57.2958
+    x = rho * np.sin(phi) * np.sin(theta)
+    y = rho * np.cos(theta)
+    z = rho * np.cos(phi) * np.sin(theta)
+    return np.round((x, y, z), Precision)
 
 
 class Coordinates:
@@ -30,28 +45,30 @@ class Coordinates:
 
     def __init__(self, *, car=None, cyl=None, pol=None):
         if car and len(car) >= 3:  # CARTESIAN: (X, Y, Z)
-            self.cartesian = (
-                car[0] * units.kilometer,
-                car[1] * units.kilometer,
-                car[2] * units.kilometer,
-            )
+            self.cartesian = (car[0], car[1], car[2])
         elif cyl and len(cyl) >= 3:  # CYLINDRICAL: (R, φ azimuth, Y)
-            pass
+            y = cyl[2]
+            x, z = polar2_cart2(cyl[0], cyl[1])
+            self.cartesian = (x, y, z)
         elif pol and len(pol) >= 3:  # SPHERICAL: (R, θ elevation, φ azimuth)
-            pass
+            self.cartesian = polar3_cart3(*pol)
         else:
             raise TypeError("Coordinates object requires initial values")
 
         self.heading = (
-            0 * units.degree,
-            0 * units.degree,
+            0 * u.degree,
+            0 * u.degree,
         )  # (θ elevation, φ azimuth); Direction object is FACING
         self.course = (
-            0 * units.degree,
-            0 * units.degree,
+            0 * u.degree,
+            0 * u.degree,
         )  # (θ elevation, φ azimuth); Direction object is MOVING
-        self.velocity = 0 * (units.meter / units.second)
+        self.velocity = 0 * (u.meter / u.second)
         # Velocity can be considered the rho of the course
+
+    @property
+    def array(self):
+        return np.array([*self.cartesian]) * u.kilometer
 
     @property
     def c_car(self):
@@ -80,7 +97,7 @@ def get_bearing(a, b):
     ac = a.c_car  # Cartesian of A
     bp = b.c_pol  # Polar of B
     bc = b.c_car  # Cartesian of B
-    ab_r = numpy.sqrt(
+    ab_r = np.sqrt(
         (ac[0] - bc[0]) ** 2 + (ac[1] - bc[1]) ** 2 + (ac[2] - bc[2]) ** 2
     )  # Rho of output
     ab_tp = ap[1] - bp[1], ap[2] - bp[2]  # Theta and Phi of output
