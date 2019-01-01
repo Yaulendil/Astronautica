3ed97ea63b6112f8ea71125c89c41e6aacf9110b
Davarice <jsdreim@gmail.com>
2019-01-01 07:24:42 -0500

    Further miscellaneous work including authentication

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ from cmd import Cmd
 import crypt
 import getpass
 import pathlib
+import random
 import time
 
 import astroio
@@ -13,12 +14,22 @@ wd = "astronautica"
 _PromptString = "{c}{u}@{h}\033[0m:\033[94m{p}\033[0m$ "
 
 
+def _delay(a=5, b=20):
+    """Wait a short time to simulate an advanced function that causes a hang"""
+
+    time.sleep(random.randint(a, b) / 10)
+
+
 def _interruptible(func):
+    """Designates a method during which a KeyboardInterrupt will not exit the shell."""
+
     def attempt(*a, **kw):
         try:
             func(*a, **kw)
         except KeyboardInterrupt:
             print("(Interrupted)")
+
+    attempt.__doc__ = func.__doc__
     return attempt
 
 
@@ -31,7 +42,7 @@ class TerminalCore(Cmd):
     def __init__(self):
         super().__init__()
         self.host = self.prompt_init
-        self.path = "~/"
+        self.path = "~"
 
     @property
     def user(self):
@@ -61,6 +72,17 @@ class TerminalCore(Cmd):
             return super().default(line)
 
 
+class TerminalHost(TerminalCore):
+    prompt_init = "Panopticon"
+    promptColor = "\033[91m"
+
+    def __init__(self, game):
+        """Attach to $game and take control of $vessel"""
+        super().__init__()
+        self.game = game
+        self.host = "{}:0".format(self.game.name)
+
+
 class TerminalGame(TerminalCore):
     prompt_init = "Starship"
     promptColor = "\033[36m"
@@ -69,38 +91,79 @@ class TerminalGame(TerminalCore):
         """Attach to $game and take control of $vessel"""
         super().__init__()
         self.game = game
-        self.vessel_name = vessel
-        self.host = self.vessel_name
+        self.vessel = vessel
+        self.host = "FleetNet.{}:{}".format(self.game.name, self.vessel.name[:-5])
+        self.path = "/command"
 
-    @property
-    def vessel(self):
-        path = pathlib.PurePath(self.game, self.vessel_name)
+        self.model = None
+        self.load()
+        self.updated = self.model["updated"]
+
+    def rescan(self):
+        path = pathlib.PurePath(self.game, self.vessel.name)
         concrete = pathlib.Path(*path.parts)
         if concrete.is_file():
             return astroio.load(concrete)
 
-    def do_load(self, line):
-        pass
+    def load(self):
+        self.model = self.rescan()
 
+    def save(self):
+        latest = self.rescan()
+        if latest["updated"] > self.model["updated"]:
+            print("Failed to commit changes: Telemetry out of date")
+            return
+        elif hash(latest) == hash(self.model):
+            print("Failed to commit changes: No Change")
+            return
 
-class TerminalHost(TerminalCore):
-    prompt_init = "Panopticon"
-    promptColor = "\033[91m"
+    def authenticate(self):
+        if not self.model["hash"]:
+            print("No password is set.")
+            self.do_passwd()
+            return True
+        else:
+            pwhash = crypt.crypt(getpass.getpass(), self.model["salt"])
+            if pwhash == self.model["hash"]:
+                return True
+            else:
+                print("Authentication failure.")
+
+    def do_update(self, *_):
+        """Retrieve the latest telemetry scans of the voidcraft and its local space."""
+        if input("Unsaved changes will be lost. Confirm? [y/N] ").lower() == "y":
+            self.load()
+            print("Reloaded.")
+
+    def do_passwd(self, *_):
+        h1 = getpass.getpass("Enter new password: ")
+        h2 = getpass.getpass("Confirm password: ")
+        if h1 != h2:
+            print("Passwords do not match.")
+            return
 
 
 class TerminalLogin(TerminalCore):
+    """The login shell. A game lobby of sorts, from which the user launches the other shells."""
+
     prompt_init = "FleetNet"
     intro = "Secure connection to FleetNet complete. For help: 'help' or '?'"
     farewell = "QLink powering down..."
     promptColor = "\033[33m"
 
+    def __init__(self):
+        super().__init__()
+        self.path = "/login"
+
     def do_ls(self, line):
+        """List currently active Host Stations, or vessels in range of a Host Station.
+        Syntax: ls [<host_name>]"""
         if line:
             path = pathlib.Path(wd, line)
-            title = "List of vessels in game '{}':".format(line)
+            title = "List of vessels in range of Host '{}':".format(line)
         else:
             path = pathlib.Path(wd)
-            title = "List of live Hosts:"
+            title = "List of live Host Stations:"
         ls = [p.stem for p in path.glob("*")]
         if not path.is_dir() or not ls:
             print("Nothing found")
@@ -108,22 +171,40 @@ class TerminalLogin(TerminalCore):
         print(title)
         for game in ls:
             if not game.endswith("_orders"):
-                print("  - " + game)
+                print("  - " + game[:-5])
 
     def do_host(self, line):
-        name = (line or input("Enter title of game: ")).strip()
+        """Tunnel to a Host Station through which interacting voidcraft can be directed.
+        Syntax: host <host>"""
+        name = (line or input("Enter title of Host Station: ")).strip()
         if not name:
             print("Invalid name.")
             return
 
+    @_interruptible
     def do_login(self, line):
         """Connect to a vessel via QSH to poll its state or issue orders.
-        Syntax: login <location>/<vessel_name>"""
-        name = (line or input("Enter game/vessel: ")).strip()
-        if not name:
+        Syntax: login <host>/<vessel_name>"""
+        name = (line or input("Enter 'host/vessel': ")).strip()
+        if not name or name.count("/") != 1:
             print("Invalid name.")
             return
-        print("Connecting to {} as {}.".format(self.user, name))
-        pwhash = getpass.getpass()
-        TerminalGame(None, None).cmdloop()
 
+        host, vessel = name.split("/")
+        hostpath = pathlib.Path(wd, host)
+        shippath = pathlib.Path(wd, host, vessel + ".json")
+        if not hostpath.is_dir():
+            print("Host Station not found")
+            return
+        elif not shippath.is_file():
+            print("Vessel not found")
+            return
+
+        print(
+            "Connecting to qsh://FleetNet.{}:{} as '{}'...".format(
+                host, vessel, self.user
+            )
+        )
+        shell = TerminalGame(hostpath, shippath)
+        if shell.authenticate():
+            shell.cmdloop()
