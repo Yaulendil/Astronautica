c75334beb9c1f2d1b39d972470f20d20c45a263b
Davarice <jsdreim@gmail.com>
2019-08-28 18:05:51 -0400

    Consolidate Prompt Class

--- astronautica/cli2/__init__.py
+++ astronautica/cli2/__init__.py
@@ -1,10 +1,9 @@
 from enum import auto, Enum
 from itertools import cycle
-from typing import Callable, Dict, List, Sequence, Tuple, Union
+from typing import Callable, Dict, Union
 
 from prompt_toolkit import Application
 from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.document import Document
 from prompt_toolkit.filters import Condition
 from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.key_binding import KeyBindings
@@ -22,19 +21,9 @@ from prompt_toolkit.widgets import HorizontalLine, VerticalLine
 
 
 STYLE = Style([("etc", ""), ("hostname", "fg:ansicyan"), ("path", "fg:ansiblue bold")])
-doc = Document("asdf@qwert~$ ")
 
 
-class Prompt:
-    def __init__(self, text: Sequence[Tuple[str, str]]):
-        self.processor = BeforeInput(self)
-        self.prompt = list(text)
-
-    def __call__(self, text: str = ""):
-        return FormattedText([*self.prompt, ("class:etc", text)])
-
-
-def line(text: Union[List[Tuple[str, str]], str]) -> Window:
+def line(text: Union[FormattedText, str]) -> Window:
     win = Window(
         FormattedTextControl(text),
         dont_extend_height=True,
@@ -42,14 +31,6 @@ def line(text: Union[List[Tuple[str, str]], str]) -> Window:
         wrap_lines=True,
     )
     return win
-
-
-scrollback = FormattedTextControl(text=[])
-cmd_panel = HSplit((line("asdf"),))
-
-
-def echo(text: FormattedText):
-    cmd_panel.children.append(line(text))
 
 
 def keys(given: Dict[str, Callable] = None, *, defaults: bool = True) -> KeyBindings:
@@ -62,8 +43,9 @@ def keys(given: Dict[str, Callable] = None, *, defaults: bool = True) -> KeyBind
             """Ctrl-Q: Exit program."""
             event.app.exit()
 
-    for k, v in given.items():
-        kb.add(k)(v)
+    if given:
+        for k, v in given.items():
+            kb.add(k)(v)
 
     return kb
 
@@ -73,6 +55,54 @@ class Mode(Enum):
     SCANS = auto()
     ORDERS = auto()
     OFF = auto()
+
+
+class Prompt:
+    def __init__(
+        self,
+        username: str,
+        host: str,
+        path: str = "~",
+        *,
+        namestyle: str = None,
+        prefix: str = "",
+    ):
+        self.username: str = username
+        self.hostname: str = host
+        self.path: str = path
+
+        self.namestyle: str = namestyle
+        self.prefix: str = prefix
+        self.processor = BeforeInput(self)
+
+        self.char: str = "$ "
+
+    # @property
+    # def char(self):
+    #     return "# " if self.username.lower() == "root" else "$ "
+
+    @property
+    def prompt(self) -> FormattedText:
+        return FormattedText(
+            [
+                ("class:etc", self.prefix),
+                (
+                    "class:hostname" if self.namestyle is None else self.namestyle,
+                    f"{self.username}@{self.hostname}",
+                ),
+                ("class:etc", ":"),
+                ("class:path", self.path),
+                ("class:etc", self.char),
+            ]
+        )
+
+    def __call__(self, text: str = None) -> FormattedText:
+        if text is None:
+            return self.prompt
+        else:
+            p = self.prompt
+            p.append(("class:etc", text))
+            return p
 
 
 class Client:
@@ -86,78 +116,74 @@ class Client:
         def nextmode(*_):
             self.state = next(mode)
 
-        self.prompt = Prompt(
-            [
-                ("class:hostname", "asdf@qwert"),
-                ("class:etc", ":"),
-                ("class:path", "~"),
-                ("class:etc", "$ "),
-            ]
-        )
+        self.prompt = Prompt("anon", "ingress", "/login")
 
-        self.app = None
         self.bar = FormattedTextControl("asdf qwert")
-        self.cmd_line = Buffer(accept_handler=self.enter, multiline=False)
+        self.cmd = Buffer(accept_handler=self.enter, multiline=False)
+        self.panel = HSplit((line("Interface ready."),))
 
         self.scope_topdown = FormattedTextControl(text="TopDown")
         self.scope_horizon = FormattedTextControl(text="Horizon")
-        self.scopes = HSplit(
-            (
-                # Top-down visualization on the upper panel.
-                Window(
-                    self.scope_topdown,
-                    ignore_content_height=True,
-                    ignore_content_width=True,
-                ),
-                HorizontalLine(),
-                # Visualization from behind on the lower panel.
-                Window(
-                    self.scope_horizon,
-                    ignore_content_height=True,
-                    ignore_content_width=True,
-                ),
-            )
-        )
         self.scans = FormattedTextControl(text="Scans")
         self.orders = FormattedTextControl(text="Orders")
+
+    def echo(self, *text: Union[FormattedText, str]):
+        lines = [line(l) for l in text]
+        self.panel.children += lines
+        return lines
 
     def enter(self, buffer: Buffer):
         command: str = buffer.text
         buffer.reset(append_to_history=True)
-        echo(self.prompt(command))
+        self.echo(self.prompt(command))
 
     def __enter__(self):
-        if self.app:
-            raise RuntimeError("Client already has an Application open.")
         root = VSplit(
             (
                 # Command History on most of the left panel, Prompt at the bottom.
                 HSplit(
                     (
-                        cmd_panel,
+                        self.panel,
                         Window(
-                            BufferControl(self.cmd_line, [self.prompt.processor]),
+                            BufferControl(self.cmd, [self.prompt.processor]),
                             wrap_lines=True,
                         ),
                     )
                 ),
-                ConditionalContainer(
+                ConditionalContainer(  # Vertical Line.
                     VerticalLine(), Condition(lambda: self.state is not Mode.OFF)
                 ),
-                ConditionalContainer(
-                    self.scopes, Condition(lambda: self.state is Mode.SCOPES)
+                ConditionalContainer(  # Scopes Panel. Visualizes nearby Space.
+                    HSplit(
+                        (
+                            # Top-down visualization on the upper panel.
+                            Window(
+                                self.scope_topdown,
+                                ignore_content_height=True,
+                                ignore_content_width=True,
+                            ),
+                            HorizontalLine(),
+                            # Visualization from behind on the lower panel.
+                            Window(
+                                self.scope_horizon,
+                                ignore_content_height=True,
+                                ignore_content_width=True,
+                            ),
+                        )
+                    ),
+                    Condition(lambda: self.state is Mode.SCOPES),
                 ),
-                ConditionalContainer(
+                ConditionalContainer(  # Scans Panel. Lists nearby Objects.
                     Window(self.scans, ignore_content_width=True),
                     Condition(lambda: self.state is Mode.SCANS),
                 ),
-                ConditionalContainer(
+                ConditionalContainer(  # Orders Panel. Shows future actions.
                     Window(self.orders, ignore_content_width=True),
                     Condition(lambda: self.state is Mode.ORDERS),
                 ),
             )
         )
-        self.app = Application(
+        return Application(
             full_screen=True,
             key_bindings=self.kb,
             layout=Layout(
@@ -167,8 +193,8 @@ class Client:
             ),
             style=STYLE,
         )
-        return self.app
 
     def __exit__(self, exc_type, exc_value, traceback):
+        pass
         # self.app.exit()
-        self.app = None
+        # self.app = None
