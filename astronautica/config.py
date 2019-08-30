38227b7dbbeed52c3d0b886f77826440ee952b43
Davarice <jsdreim@gmail.com>
2019-08-30 00:04:47 -0400

    Implement Configuration Module

--- astronautica/config.py
+++ astronautica/config.py
@@ -9,3 +9,50 @@ class Scan:
     indent = 3
     display_attr = ["radius", "mass", "coords"]
     decimals = 3
+
+
+from pathlib import Path
+from sys import argv
+
+import yaml
+
+
+DELIM = "/"
+
+
+def getpath(filename: str = "config.yml"):
+    p = Path(argv[0])
+
+    if p.is_file():
+        p = p.parent / filename
+    elif p.is_dir():
+        p /= filename
+    else:
+        raise FileNotFoundError(p)
+
+    return p
+
+
+class Config(object):
+    def __init__(self, path: Path = getpath()):
+        self.path: Path = path
+        with self.path.open("r") as file:
+            self.data = yaml.safe_load(file)
+
+    def get(self, route: str, default = None):
+        if DELIM in route:
+            route = route.split(DELIM)
+        else:
+            route = [route]
+
+        here = self.data
+        try:
+            for jump in filter(None, route):
+                here = here[jump]
+        except:
+            return default
+        else:
+            return here
+
+
+cfg = Config()
