5373fff7c24b51a97399087549633e16ec14c65f
Davarice <jsdreim@gmail.com>
2019-09-05 03:42:13 -0400

    Reimplement Console again, this time based on a modified PTTerm

--- astronautica/interface/client.py
+++ astronautica/interface/client.py
@@ -1,12 +1,12 @@
 from asyncio import AbstractEventLoop, Task
 from enum import auto, Enum
-from itertools import chain, cycle
-from typing import Callable, List, Optional, Union, Iterable, Tuple
+from itertools import cycle
+from typing import Callable, List, Optional, Union
 
 from prompt_toolkit import Application
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.filters import Condition
-from prompt_toolkit.formatted_text import FormattedText, split_lines
+from prompt_toolkit.formatted_text import FormattedText, fragment_list_to_text
 from prompt_toolkit.layout.containers import (
     ConditionalContainer,
     HSplit,
@@ -17,8 +17,9 @@ from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.layout.processors import BeforeInput
 from prompt_toolkit.widgets import HorizontalLine, VerticalLine
+from ptterm import Terminal
 
-from .etc import fmt, keys, N, STYLE
+from .etc import keys, STYLE, T, unformat
 from .execution import execute_function
 
 
@@ -81,66 +82,6 @@ class Prompt(object):
             return p
 
 
-def wrap(text: Iterable[Tuple[str, str]], width: int) -> int:
-    plain = "".join(line[1] for line in text)
-    # run_in_terminal(partial(print, width, text, plain, sep="\n"))
-    return int(len(plain) / width) + 1
-
-
-class Panel(object):
-    def __init__(self):
-        self._text = [[("class:etc", "Ready")]] # deque(
-            # [[("class:etc", "Ready")]], maxlen=200
-        # )  # [[("class:etc", "Ready")]]
-        self.text = FormattedText(self._text[0].copy())
-        self.ftc = FormattedTextControl(
-            self.text,
-            # get_cursor_position=(lambda: Point(5, 0)),
-        )
-        # self.ftc = TextArea("asdf", focusable=False)
-        self.window = Window(
-            self.ftc,
-            dont_extend_height=True,
-            ignore_content_width=True,
-            # scroll_offsets=ScrollOffsets(bottom=90000000),
-            wrap_lines=True,
-        )
-        # self.ftc.get_cursor_position = lambda: Point(5, (self.window.height or 1) - 1)
-
-    def resize(self):
-        # Chain the last X lines and set the "effective" text to that.
-        h = self.window.height or 17
-        w = self.window.width or 59
-
-        buffer = []
-        rows_to_render = 0
-        text = filter(None, reversed(self._text))
-
-        while rows_to_render < h:
-            try:
-                line = next(text)
-            except:
-                break
-
-            rows_to_render += wrap(line, w)
-            buffer.append(line)
-
-        buffer = list(reversed(buffer))
-        while len(buffer) < rows_to_render:
-            buffer.append([])
-        # run_in_terminal(partial(print, [(l, wrap(l, w), w) for l in buffer]))
-
-        self.text[:] = tuple(
-            chain(*(N + list(l) for l in (buffer[max(len(buffer) - h, 0):])))
-        )[1:]
-
-    def write(self, *text):
-        for line in text:
-            # Extend the internal text with each line from a split_lines() call.
-            self._text.extend(split_lines(line))
-        self.resize()
-
-
 class Client(object):
     def __init__(self, loop: AbstractEventLoop, command_handler: Callable = None):
         self.LOOP: AbstractEventLoop = loop
@@ -166,8 +107,8 @@ class Client(object):
             multiline=False,
             read_only=Condition(lambda: self.read_only),
         )
-        self.panel = Panel()
-        # sys.stdout = self.panel
+        self.term = Terminal(sim_prompt=True)
+        self.console = self.term.terminal_control.process.terminal
 
         self.procs = [self.prompt.processor]
         self._procs = self.procs.copy()
@@ -181,6 +122,9 @@ class Client(object):
         self.handler = command_handler
         self._app: Optional[Application] = None
 
+        self.console.write_text("Ready.")
+        self.console.ready()
+
     def cmd_hide(self):
         self.read_only = True
         self._procs.clear()
@@ -189,11 +133,13 @@ class Client(object):
         self.read_only = False
         self._procs[:] = self.procs
 
-    def echo(self, *text: Union[FormattedText, str]):  # -> List[Window]:
-        # lines = list(filter(None, (line(l) for l in text)))
-        self.panel.write(*map(fmt, text))
+    def echo(self, *text: Union[FormattedText, str]):
+        for line in text:
+            if isinstance(line, FormattedText):
+                line = fragment_list_to_text(line)
+
+            self.console.write_text("\r\n" + str(line))
         self.redraw()
-        # return lines
 
     def enter(self, buffer: Buffer) -> None:
         try:
@@ -206,68 +152,74 @@ class Client(object):
             self.cmd_show()
 
     def execute(self, command: str) -> None:
-        self.echo(self.prompt(command))
+        self.echo(unformat(self.prompt(command)))
+        self.redraw()
 
         if callable(self.handler):
             execute_function(command, self.echo, self.handler, self.LOOP, self.TASKS)
 
     def redraw(self) -> None:
+        self.console.ready()
         if self._app:
             self._app.renderer.render(self._app, self._app.layout)
 
     def __enter__(self) -> Application:
-        root = VSplit(
-            (
-                # Command History on most of the left panel, Prompt at the bottom.
-                HSplit(
-                    (
-                        self.panel.window,
-                        Window(BufferControl(self.cmd, self._procs), wrap_lines=True),
-                    )
-                ),
-                ConditionalContainer(  # Vertical Line.
-                    VerticalLine(), Condition(lambda: self.state is not Mode.OFF)
-                ),
-                ConditionalContainer(  # Scopes Panel. Visualizes nearby Space.
-                    HSplit(
+        root = Layout(
+            HSplit(
+                (
+                    Window(self.bar, height=1, style="ansigray bold reverse"),
+                    VSplit(
                         (
-                            # Top-down visualization on the upper panel.
-                            Window(
-                                self.scope_topdown,
-                                ignore_content_height=True,
-                                ignore_content_width=True,
+                            # Command History on most of the left panel, Prompt at the bottom.
+                            HSplit(
+                                (
+                                    self.term,
+                                    Window(
+                                        BufferControl(self.cmd, self._procs),
+                                        wrap_lines=True,
+                                        height=1,
+                                    ),
+                                )
                             ),
-                            HorizontalLine(),
-                            # Visualization from behind on the lower panel.
-                            Window(
-                                self.scope_horizon,
-                                ignore_content_height=True,
-                                ignore_content_width=True,
+                            ConditionalContainer(  # Vertical Line.
+                                VerticalLine(),
+                                Condition(lambda: self.state is not Mode.OFF),
+                            ),
+                            ConditionalContainer(  # Scopes Panel. Visualizes nearby Space.
+                                HSplit(
+                                    (
+                                        # Top-down visualization on the upper panel.
+                                        Window(
+                                            self.scope_topdown,
+                                            ignore_content_height=True,
+                                            ignore_content_width=True,
+                                        ),
+                                        HorizontalLine(),
+                                        # Visualization from behind on the lower panel.
+                                        Window(
+                                            self.scope_horizon,
+                                            ignore_content_height=True,
+                                            ignore_content_width=True,
+                                        ),
+                                    )
+                                ),
+                                Condition(lambda: self.state is Mode.SCOPES),
+                            ),
+                            ConditionalContainer(  # Scans Panel. Lists nearby Objects.
+                                Window(self.scans, ignore_content_width=True),
+                                Condition(lambda: self.state is Mode.SCANS),
+                            ),
+                            ConditionalContainer(  # Orders Panel. Shows future actions.
+                                Window(self.orders, ignore_content_width=True),
+                                Condition(lambda: self.state is Mode.ORDERS),
                             ),
                         )
                     ),
-                    Condition(lambda: self.state is Mode.SCOPES),
-                ),
-                ConditionalContainer(  # Scans Panel. Lists nearby Objects.
-                    Window(self.scans, ignore_content_width=True),
-                    Condition(lambda: self.state is Mode.SCANS),
-                ),
-                ConditionalContainer(  # Orders Panel. Shows future actions.
-                    Window(self.orders, ignore_content_width=True),
-                    Condition(lambda: self.state is Mode.ORDERS),
-                ),
+                )
             )
         )
-        self._app = Application(
-            Layout(
-                HSplit(
-                    (Window(self.bar, height=1, style="ansigray bold reverse"), root)
-                )
-            ),
-            STYLE,
-            full_screen=True,
-            key_bindings=self.kb,
-        )
+        root.focus(self.cmd)
+        self._app = Application(root, STYLE, full_screen=True, key_bindings=self.kb)
         return self._app
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
--- astronautica/interface/etc.py
+++ astronautica/interface/etc.py
@@ -1,6 +1,7 @@
 from itertools import repeat
 from typing import Any, Callable, Dict, Union
 
+from blessings import Terminal
 from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.styles import Style
@@ -13,12 +14,65 @@ N = FormattedText([("class:etc", "\n")])
 NNN = repeat(("class:etc", "\n"))
 STYLE = Style(list(cfg.get("interface/style").items()))
 
+T = Terminal()
+
+
+noact = lambda x: x
+unstyle = {
+    "class:etc": noact,
+    "class:hostname": T.cyan,
+    "class:path": T.bold_bright_blue,
+}
+
 
 def fmt(text: Union[FormattedText, str], style: str = "class:etc") -> FormattedText:
     if isinstance(text, FormattedText):
         return text
     else:
         return FormattedText([(style, str(text))])
+
+
+attrs = ("bold", "italic", "reverse")
+
+
+def resolve(style: str):
+    fg = ""
+    fg_attr = set()
+    bg = ""
+    bg_attr = set()
+    terms = style.split()
+
+    for term in terms:
+        if term in attrs:
+            if fg:
+                bg_attr.add(term)
+            else:
+                fg_attr.add(term)
+
+        elif ":" in term:
+            g, color = term.split(":")
+
+            if color.startswith("ansi"):
+                color = color[4:]
+
+            if g == "fg":
+                fg = color
+            elif g == "bg":
+                bg = color
+
+    try:
+        return getattr(
+            T,
+            "_".join(
+                filter(None, (*fg_attr, fg, *(("on", *bg_attr, bg) if bg else ())))
+            ),
+        )
+    except:
+        return noact
+
+
+def unformat(text: FormattedText) -> str:
+    return "".join((unstyle.get(style) or resolve(style))(line) for style, line in text)
 
 
 def keys(
