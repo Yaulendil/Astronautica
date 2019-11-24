6e0481a3651d48b8735799956aa82e4b1aa9d74a
Davarice <jsdreim@gmail.com>
2019-11-23 20:16:50 -0500

    Implement Key based Registration

--- astronautica/users/__init__.py
+++ astronautica/users/__init__.py
@@ -1,6 +1,7 @@
+from datetime import datetime as dt
 from pathlib import Path
 from secrets import token_hex
-from typing import Dict
+from typing import Dict, Iterator, NewType, Optional, Type
 
 from ezipc.remote import Remote
 from passlib.hash import pbkdf2_sha512 as pwh
@@ -9,46 +10,58 @@ from config import cfg
 from util.storage import PersistentDict
 
 
-check_token = lambda tk, i: int(tk.replace("-", ""), base=16) == i
+AccessKey: Type[str] = NewType("Access Key", str)
+
+keys: Dict[AccessKey, Optional[Dict[str, str]]] = PersistentDict(
+    Path(cfg["data/directory"], "KEYS").with_suffix(".json"), fmt="json"
+)
 logins: Dict[str, "Session"] = {}
 
 
-def generate_token():
-    tk = token_hex(12).upper()
-    o = []
-
-    while tk:
-        o.append(tk[:4])
-        tk = tk[4:]
-
-    return "-".join(tk)
+def generate_token(chunks: int = 3, size: int = 5, div: str = "-") -> AccessKey:
+    tk = token_hex(chunks * size).upper()
+    return AccessKey(div.join(tk[i * size : (i + 1) * size] for i in range(chunks)))
 
 
-def get_user(name: str):
+def get_user(name: str, make: bool = False):
     path = Path(cfg["data/directory"], "users", name).with_suffix(".json")
+
     if path.is_file():
         return PersistentDict(path, fmt="json")
+    elif make:
+        return new_user(name)
+    else:
+        raise FileNotFoundError
 
 
-def new_user(name: str):
+def new_keys(n: int = 1) -> Iterator[AccessKey]:
+    with keys:
+        for _ in range(n):
+            tk = generate_token()
+            keys[tk] = None
+            yield tk
+
+
+def new_user(name: str, exist_ok: bool = True):
     path = Path(cfg["data/directory"], "users", name).with_suffix(".json")
 
-    if path.exists():
+    if path.exists() and not exist_ok:
         raise FileExistsError
     else:
-        return PersistentDict(path, flag="n", fmt="json")
+        return PersistentDict(path, fmt="json")
 
 
 class Session(object):
     def __init__(self, remote: Remote):
         self.remote: Remote = remote
-        self.user = None
+        self.user: Optional[PersistentDict] = None
 
+    @property
     def active(self) -> bool:
         return self.remote.open
 
     def login(self, username: str, password: str) -> bool:
-        user = get_user(username)
+        user = get_user(username, False)
         if user:
             if pwh.verify(password, user.get("password")):
                 self.user = user
@@ -64,7 +77,13 @@ class Session(object):
             del logins[self.user.path.stem]
             self.user = None
 
-    def register(self, username: str, password: str, key: str):
-        # TODO: Require valid Key Token before allowing Registration.
-        user = new_user(username)
-        user["password"] = pwh.hash(password)
+    def register(self, username: str, password: str, key: AccessKey):
+        with keys:
+            if key in keys and keys[key] is None:
+                keys[key] = dict(time=dt.utcnow().isoformat(), username=username)
+
+                with new_user(username, False) as user:
+                    user["password"] = pwh.hash(password)
+                    user["key"] = key
+            else:
+                raise PermissionError("Invalid Key")
